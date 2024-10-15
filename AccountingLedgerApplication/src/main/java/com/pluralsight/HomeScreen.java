b804bcf6bebf97b04f9e884639606370c64de72f
Akbar <akbarsaidakramov@gmail.com>
2024-10-15 13:16:12 -0700

    adding makePayment method

--- AccountingLedgerApplication/src/main/java/com/pluralsight/HomeScreen.java
+++ AccountingLedgerApplication/src/main/java/com/pluralsight/HomeScreen.java
@@ -13,9 +13,10 @@ public class HomeScreen {
     private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) throws IOException {
         //userOptions();
-        addDeposit();
+        //addDeposit();
         //System.out.println(randomDate());
         //System.out.println(randomTime());
+        makePayment();
 
     }
 
@@ -51,9 +52,9 @@ public class HomeScreen {
         //LocalDate date = LocalDate.parse(input("Please enter the date in YYYY-MM-DD format. "));
         LocalTime time = randomTime();
         //LocalTime time = LocalTime.parse(input("Please enter the time in HH:mm:ss format "));
-        String description = input("Please enter the description of item. ");
+        String description = input("Please enter the description of the deposit. ");
         String vendor = input("Please enter the vendor name. ");
-        double amount = Double.parseDouble(input("Please enter the amount paid. "));
+        double amount = Double.parseDouble(input("Please enter the amount deposited. "));
         //constructing AddDeposit object
         AddDeposit deposit = new AddDeposit(date, time, description, vendor, amount);
         //adding object to a list
@@ -78,6 +79,50 @@ public class HomeScreen {
         }
 
     }
+
+    public static void makePayment() throws IOException {
+        //saving users input to an answer variable
+        String answer = userOptions();
+
+        if (!answer.equalsIgnoreCase("P")){
+            return;
+        }
+        //creating a list
+        List<AddDeposit> addDeposits = new ArrayList<>();
+        //creating a variables using user input
+        LocalDate date = randomDate();
+        //LocalDate date = LocalDate.parse(input("Please enter the date in YYYY-MM-DD format. "));
+        LocalTime time = randomTime();
+        //LocalTime time = LocalTime.parse(input("Please enter the time in HH:mm:ss format "));
+        String description = input("Please enter the description of the item. ");
+        String vendor = input("Please enter the vendor name. ");
+        double amount = Double.parseDouble(input("Please enter the amount paid. "));
+        //constructing AddDeposit object
+        AddDeposit deposit = new AddDeposit(date, time, description, vendor, amount * -1);
+        //adding object to a list
+        addDeposits.add(deposit);
+
+        try{
+            //create a BufferedWriter
+            BufferedWriter writer = new BufferedWriter(new FileWriter("transactions.csv", true));
+            //if file is empty create a header
+            File file = new File("transactions.csv");
+            if (file.length() == 0) {
+                writer.write("date|time|description|vendor|amount\n");
+            }
+            //write deposit info to the file
+            for (AddDeposit a : addDeposits) {
+                writer.write(a.toCsvLine());
+            }
+
+            writer.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+
 
     public static LocalDate randomDate(){
         long startDate = LocalDate.now().minusYears(1).toEpochDay();
